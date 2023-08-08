19a9cead220e964ee783b9d46fd706ab885e129a
Max Ohsawa <max.ohsawa@gmail.com>
2023-08-08 14:38:52 -0400

    adds mongoose connection logic, standby for config with railway mongodb service

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const db = require('./config/connection')
 
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -8,6 +9,9 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static(path.join('..', 'client', 'dist')))
 
-app.listen(PORT, () => {
-  console.log(`Express server listening to port ${PORT}`)
+db.once('open', () => {
+  
+  app.listen(PORT, () => {
+    console.log(`Express server listening to port ${PORT}`)
+  })
 })

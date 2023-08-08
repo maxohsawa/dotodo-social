ce20d5e4d6018a198d12e3bea8cba61b8051a662
Max Ohsawa <max.ohsawa@gmail.com>
2023-08-08 14:59:29 -0400

    fixes mongo url variable name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-mongoose.connect(`${process.env.MONGO_URL}/dotodo-social` || 'mongodb://127.0.0.1:27017/dotodo-social')
+mongoose.connect(process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/dotodo-social')
 
 module.exports = mongoose.connection

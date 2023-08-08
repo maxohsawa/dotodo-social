7a5e4caa7c73485d0bc47446df96890ba1acfc86
Max Ohsawa <max.ohsawa@gmail.com>
2023-08-08 17:38:14 -0400

    adds apollo graphql client to react client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,41 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client'
+
+import { setContext } from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        dotodo social
-      </h1>
+      <ApolloProvider client={client}>
+        <h1 className="text-3xl font-bold underline">
+          dotodo social
+        </h1>
+      </ApolloProvider>
     </>
   )
 }

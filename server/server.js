cd1c52c4e0e0154ab38554a0d99190059b6083e4
Max Ohsawa <max.ohsawa@gmail.com>
2023-08-08 13:55:39 -0400

    adds express server, adds install, dev, build, deploy scripts to root

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+
+const PORT = process.env.PORT || 3001
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static(path.join('..', 'client', 'dist')))
+
+app.listen(PORT, () => {
+  console.log(`Express server listening to port ${PORT}`)
+})

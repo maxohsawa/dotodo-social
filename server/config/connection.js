9b997f5944f3307032718d16d6104c9dd7f69c47
Max Ohsawa <max.ohsawa@gmail.com>
2023-08-08 14:44:56 -0400

    configures mongoose connection string to work with railway

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dotodo-social')
+mongoose.connect(`${process.env.MONGODB_URL}/dotodo-social` || 'mongodb://127.0.0.1:27017/dotodo-social')
 
 module.exports = mongoose.connection

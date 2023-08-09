61a973417f43f3fe6db1748717b9c008c55fc001
Max Ohsawa <max.ohsawa@gmail.com>
2023-08-08 20:47:08 -0400

    adds basic login card component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,83 @@
-import { Box, Input } from '@chakra-ui/react'
+import { useState } from 'react'
+
+import { 
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+  Text
+ } from '@chakra-ui/react'
 
 const Login = () => {
+
+  const [ formState, setFormState ] = useState({
+    email: '',
+    password: ''
+  })
+
+  const [ errorState, setErrorState ] = useState( {
+    email: false,
+    password: false
+  })
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setFormState({
+      ...formState,
+      [name]: value
+    })
+  }
+
+  const handleLogin = (event) => {
+    console.log('login attempt with', formState)    
+  }
+
   return (
     <>
-      <Box>
-        <Input placeholder='Username'/>
-      </Box>
+      <Card maxW='sm'>
+        <CardBody>
+          <Stack mt='6' spacing='3'>
+            <Text fontSize='5xl'>Login</Text>
+            <FormControl>
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  type='email'
+                  placeholder='Email address'
+                  name='email'
+                  value={formState.username}
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <FormControl isInvalid={errorState.password}>
+                <FormLabel>Password</FormLabel>
+                <Input 
+                  type='password'
+                  placeholder='Password'
+                  name='password'
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+              </FormControl>
+          </Stack>
+        </CardBody>
+        <CardFooter>
+          <ButtonGroup>
+            <Button 
+              variant='solid' 
+              colorScheme='blue'
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
     </>
   )
 }
